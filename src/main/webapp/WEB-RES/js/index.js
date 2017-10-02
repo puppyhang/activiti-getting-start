036005cd9fff305fa7dd34897613d0cd25e28dfe
陶江航 <taojianghang@xinzhentech.com>
2017-10-03 00:19:10 +0800

    添加logo以及前当部署成功之后关闭弹窗

--- src/main/webapp/WEB-RES/js/index.js
+++ src/main/webapp/WEB-RES/js/index.js
@@ -10,28 +10,24 @@ var vue = new Vue({
 				type : 1,
 				skin : 'layui-layer-rim',
 				area : [ '500px', '300px' ],
-				// type 位 为 1 的时候加载内容的方式
-				// content : $("#deploy-pane")
-				// 内容为2的时候会以一个http请求的方式加载
-				// '/res'是spring的静态资源映射
+				// type 位 为 1 的时候加载内容的方式content :
+				// $("#deploy-pane"),type为2的时候会以一个http请求的方式加载
 				content : $("#deploy-pane"),
 				btn : [ "部署", "取消" ],
-				// index唯一标识这个弹出层
 				yes : function(index, layero) {
 					// 提交表单
 					var form = $(layero).find("form");
-					alert("ajax表单提交" + form);
 					form.ajaxSubmit({
 						type : 'POST',
 						url : '/process/def/deploy',
 						dataType : 'json',
 						success : function(data) {
 							if (data && data['status'] === "200") {
-								layer.close(index);
 								layer.msg(data['message']);
 							} else {
 								layer.msg(data['message']);
 							}
+							layer.close(index);
 						},
 						clearForm : true,
 						resetForm : true
